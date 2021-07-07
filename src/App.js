453a2fd37aa1d56441a1c7a426d0a8a0ba573897
Sha Mwe La <shamwela2002@gmail.com>
2021-07-07 13:00:04 +0630

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
-function App() {
-  return <></>
+import React, { Component } from 'react'
+class App extends Component {
+  state = {
+    result: null,
+    min: 1,
+    max: 10,
+  }
+
+  componentDidMount() {
+    this.handleSubmit()
+  }
+
+  handleChange = ({ currentTarget }) => {
+    const { name, value } = currentTarget
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = () => {
+    const { min, max } = this.state
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
+    this.setState({ result })
+  }
+
+  render() {
+    const { handleSubmit, handleChange } = this
+    const { result, min, max } = this.state
+
+    return (
+      <div id='app'>
+        <h1>Random Number</h1>
+        <form onSubmit={(event) => handleSubmit(event.preventDefault)}>
+          <h2>Result: {result}</h2>
+
+          <label htmlFor='min'></label>
+          <input
+            value={min}
+            onChange={handleChange}
+            name='min'
+            id='min'
+            type='number'
+            min='1'
+            max='1000'
+          ></input>
+
+          <label htmlFor='max'></label>
+          <input
+            value={max}
+            onChange={handleChange}
+            name='max'
+            id='max'
+            type='number'
+            min='1'
+            max='1000'
+          ></input>
+
+          <button type='submit'>Generate</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App
